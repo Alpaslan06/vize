0e8bce83e7565041a7ecae56307f1130f3fab178
alpaslan10c@outlook.com <alpaslan10c@outlook.com>
2020-05-21 20:03:34 +0300

    açıklamar eklendi

--- VizeSinav/Program.cs
+++ VizeSinav/Program.cs
@@ -15,15 +15,15 @@ namespace VizeSinav
             {
                 while (true)
                 {
-                    Console.WriteLine("Kitap Ekle : 1\nKitap Listesini görüntüle : 2\nUygulamayı kapat : 3\nLütfen kitap basım tarihini yıl-ay-gün şeklinde giriniz.");
-                    Console.Write("Hoşgeldiniz.Lütfen yapmak istediğiniz işlemin numarasını giriniz : ");
+                    Console.WriteLine("Kitap Ekle : 1\nKitap Listesini görüntüle : 2\nUygulamayı kapat : 3\nLütfen kitap basım tarihini yıl-ay-gün şeklinde giriniz.(Ornek : 2020-06-05 vb)\n");
+                    Console.Write("Hoşgeldiniz.Lütfen yapmak istediğiniz işlemin numarasını giriniz : ");                                 
                     int islemno = int.Parse(Console.ReadLine());//islem secebılmek ıcın girdi alındı
-
                     //koşullar 
                     if (islemno == 1)
                     {
                         Console.Write("Lütfen girmek istediğiniz kitap sayısını yazınız : ");
                         int ksayisi = int.Parse(Console.ReadLine());//sayı girisi
+
                         Kitap[] Kitaplar = new Kitap[ksayisi];//kitap türünde dizinin uzunlugu kullanıcının gırdıgı sayı
                         int i = 0;
                         do
@@ -38,28 +38,27 @@ namespace VizeSinav
                             if (2020 < kitap.BasimTarihi.Year)//tarih kontrol
                             {
                                 Console.WriteLine("2020 den büyük olamaz. Lütfen tekrar giriniz.");
-                                continue;//bu komuttan sonra  yazılan komutlar calısmaz*                               
+                                continue;//bu komuttan sonra  bir sonraki işlem için başa döner                              
                             }
                             Console.Write("Türü : ");
                             kitap.Tur = Console.ReadLine();
 
-                            Kitaplar[i] = kitap;//degerler metot aracılıgıyla atanıyor
+                            Kitaplar[i] = kitap;//diziye degerler metot aracılıgıyla atanıyor
                             i++;
-                        } while (i < ksayisi);//for da dongu sayısını degıstıremeyız bu yuzden yanlıs gırılme durumunda 3 kitap yerine 2 kitap kaydolmasının onune gecmek ıstedım
+                        } while (i < ksayisi);//do while ile ,for da dongu sayısını degıstıremedıgımız için  yanlıs gırılme durumunda 3 kitap yerine 2 kitap kaydolmasının onune gecmek ıstedım
 
                         Kitap.dosyayaekle(Kitaplar);
                         Console.WriteLine("Kitaplar başarıyle eklendi !!!");
                     }
                     else if (islemno == 2)
                     {//okuma ıslemlerı yapılacak
-                        string dosya_yolu = @"D:\metinbelgesi.txt";
+                        string dyolu = @"D:\metinbelgesi.txt";
                         string line;
-                        StreamReader file = new StreamReader(dosya_yolu);
+                        StreamReader file = new StreamReader(dyolu);
                         while ((line = file.ReadLine()) != null)//boş değilse yazdır
                         {
                             Console.WriteLine(line);
                         }
-
                         file.Close();//dosya kapat
                     }
                     else
@@ -72,7 +71,7 @@ namespace VizeSinav
             }//hata kontrolleri
             catch (FormatException)
             {
-                Console.WriteLine("Lütfen sayı giriniz !");
+                Console.WriteLine("Lütfen sayı giriniz \t!");
             }
             catch (Exception)
             {
@@ -83,7 +82,7 @@ namespace VizeSinav
     }
     class Kitap
     {
-        //alanlar
+        //alanlar propfull kulandım
         private string _yazari;
         public string Yazar
         {
@@ -108,26 +107,26 @@ namespace VizeSinav
 
         public Kitap(string kitapAdi, string yazari, string turu, DateTime tarih)
         {
-            //metot overload
+            //metot overload,parametre değerleri atandı
             _kitapAdi = kitapAdi;
             _yazari = yazari;
             _turu = turu;
             BasimTarihi = tarih;
         }
-        public static void dosyayaekle(Kitap[] kitaplar)
+        public static void dosyayaekle(Kitap[] kitaplar)//ekleme metodu
         {
             try
             {
-                string dosya_yolu = @"D:\metinbelgesi.txt";
+                string dyolu = @"D:\metinbelgesi.txt";
                 //İşlem yapacağımız dosyanın yolunu belirtiyoruz.Sureklı yazmaktansa degıskene bır kere aktarıp her yerde kullanmak daha kolay ve pratık geldı
-                bool s = File.Exists(dosya_yolu);
-                FileStream fs = new FileStream(dosya_yolu, FileMode.Append, FileAccess.Write);
-                //file stream nesnesi oluşturdum, 1.parametre dosya yolunu,
-                //2.parametre dosya varsa açar yoksa olusturur
-                //3.parametre dosyaya yazdırma işlemi için ulasılacagını belırtır
+                bool varmi = File.Exists(dyolu);
+                FileStream fs = new FileStream(dyolu, FileMode.Append, FileAccess.Write);
+                //Bir file stream nesnesi oluşturdm. 1.parametre dosya yolunu belirtir
+                //2.parametre dosya varsa açılacağını yoksa oluşturulacağını belirtir,
+                //3.parametre dosyaya erişimin veri yazmak için olacağını gösterir.
                 StreamWriter sw = new StreamWriter(fs);
                 //Yazma işlemi için bir StreamWriter nesnesi oluşturduk.
-                if (s == false) sw.WriteLine("KİTAP ADI     KİTAP YAZARI     KİTAP TÜRÜ   BASIM YILI");
+                if (varmi == false) sw.WriteLine("KİTAP ADI     KİTAP YAZARI     KİTAP TÜRÜ   BASIM YILI");
                 //dosya var mı yok mu kontrol edecek ona göre de kitap adı kitap yazarı tarzında bir kolon oluşacak buradakı amac her eklenen kıtap ardından bu sekılde kolon oluşmasını önlemek
                 foreach (var kitap in kitaplar)
                 {
@@ -138,6 +137,7 @@ namespace VizeSinav
                     sw.Write("\r\nKitabın basım yılı : " + kitap.BasimTarihi.ToShortDateString() + "\n\n");//*
                 }
                 sw.Flush();
+                //Veriyi tampon bölgeden dosyaya aktardık.
                 sw.Close();
                 fs.Close();
                 //burada nesnelerimizi kapattık.
@@ -148,8 +148,6 @@ namespace VizeSinav
                 //hata meydana gelırse uyarı verecek e.message sayesinde ise hatanın ne oldugunu programcılar anlayabılır.
             }
         }
-
     }
-
 }
 
